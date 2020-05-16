c696b07bf5204e27eba29be8da5a8ef6ed1f7d6e
sags20 <44414301+sags20@users.noreply.github.com>
2020-05-16 16:17:56 -0600

    vistas
    vistas

--- app/src/main/java/com/XD/fitgain/LoggedOut.kt
+++ app/src/main/java/com/XD/fitgain/LoggedOut.kt
@@ -9,14 +9,14 @@ import android.view.ViewGroup
 /**
  * A simple [Fragment] subclass.
  */
-class CompleteScreen : Fragment() {
+class LoggedOut : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_complete_screen, container, false)
+        return inflater.inflate(R.layout.fragment_logged_out, container, false)
     }
 
 }

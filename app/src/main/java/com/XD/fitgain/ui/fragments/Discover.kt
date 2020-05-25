f5afed5effb44b324fd3eca0fd4f66a7a4ee16bc
Fabricio Hernandez <fabricioenrique123@gmail.com>
2020-05-25 12:18:33 -0600

    UI - Discover Selected

--- app/src/main/java/com/XD/fitgain/ui/fragments/Discover.kt
+++ app/src/main/java/com/XD/fitgain/ui/fragments/Discover.kt
@@ -1,5 +1,6 @@
 package com.XD.fitgain.ui.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -7,18 +8,35 @@ import android.view.View
 import android.view.ViewGroup
 
 import com.XD.fitgain.R
+import com.XD.fitgain.databinding.FragmentDiscoverBinding
+import com.XD.fitgain.ui.DiscoverSelected
+import com.XD.fitgain.ui.NavigationContainerHome
 
 /**
  * A simple [Fragment] subclass.
  */
 class Discover : Fragment() {
 
+    private lateinit var binding: FragmentDiscoverBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_discover, container, false)
+        binding = FragmentDiscoverBinding.inflate(inflater, container, false)
+
+        binding.cardviewRestaurante.setOnClickListener {
+            startActivityForResult(
+                Intent(
+                    requireActivity(),
+                    DiscoverSelected::class.java
+                ), 777
+            )
+        }
+
+
+
+        return binding.root
     }
 
 }

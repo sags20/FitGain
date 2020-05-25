149b2ce0452e440c81e39c4784594055cd446e58
Fabricio Hernandez <fabricioenrique123@gmail.com>
2020-05-24 22:07:30 -0600

    Add Forgot Password Screen

--- app/src/main/java/com/XD/fitgain/screens/fragments/ForgotPassword.kt
+++ app/src/main/java/com/XD/fitgain/screens/fragments/ForgotPassword.kt
@@ -6,27 +6,27 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.findNavController
-import com.XD.fitgain.R
-import com.XD.fitgain.databinding.FragmentLogInBinding
 
-class LogIn : Fragment() {
-    private lateinit var binding: FragmentLogInBinding
+import com.XD.fitgain.R
+import com.XD.fitgain.databinding.FragmentForgotPasswordBinding
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class ForgotPassword : Fragment() {
+
+    private lateinit var binding: FragmentForgotPasswordBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentLogInBinding.inflate(inflater, container, false)
+        binding = FragmentForgotPasswordBinding.inflate(inflater, container, false)
 
         binding.btnBackScreen.setOnClickListener {
-            it.findNavController().navigate(R.id.action_logIn_to_logOut)
-        }
-
-        binding.btnForgotPassword.setOnClickListener {
-            it.findNavController().navigate(R.id.action_logIn_to_forgotPassword)
+            it.findNavController().navigate(R.id.action_forgotPassword_to_logIn)
         }
 
         return binding.root
     }
-
 
 }

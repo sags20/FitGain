acb662defefebe405ae2be4c4b6f2c548911f88a
Fabricio Hernandez <fabricioenrique123@gmail.com>
2020-05-25 11:44:03 -0600

    UI - Fragment Discover

---
+++ app/src/main/java/com/XD/fitgain/ui/fragments/Discover.kt
@@ -0,0 +1,24 @@
+package com.XD.fitgain.ui.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.XD.fitgain.R
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class Discover : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_discover, container, false)
+    }
+
+}

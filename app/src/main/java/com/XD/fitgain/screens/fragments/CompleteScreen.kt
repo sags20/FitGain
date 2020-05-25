fd96ba6b9bb9e5808d33d8e43e340b636be251e5
Fabricio Hernandez <fabricioenrique123@gmail.com>
2020-05-24 22:16:00 -0600

    Add navegation Complete Screen -> Config 1

--- app/src/main/java/com/XD/fitgain/screens/fragments/CompleteScreen.kt
+++ app/src/main/java/com/XD/fitgain/screens/fragments/CompleteScreen.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
 import com.XD.fitgain.R
 import com.XD.fitgain.databinding.FragmentCompleteScreenBinding
 
@@ -16,6 +17,10 @@ class CompleteScreen : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentCompleteScreenBinding.inflate(inflater, container, false)
+
+        binding.buttonGetStarted.setOnClickListener {
+            it.findNavController().navigate(R.id.action_completeScreen_to_config1)
+        }
 
         return binding.root
     }

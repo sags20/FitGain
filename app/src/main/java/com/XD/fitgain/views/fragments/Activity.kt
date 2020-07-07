a0028c340a36e4741f4691d02b41f6b787d34f20
Fabricio Hernandez <fabricioenrique123@gmail.com>
2020-07-07 12:05:56 -0600

    Add: Calc calories, distance, points conversion

--- app/src/main/java/com/XD/fitgain/views/fragments/Activity.kt
+++ app/src/main/java/com/XD/fitgain/views/fragments/Activity.kt
@@ -1,28 +1,32 @@
 package com.XD.fitgain.views.fragments
 
+import android.R
+import android.app.AlertDialog
 import android.content.*
 import android.content.Context.MODE_PRIVATE
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.XD.fitgain.R
+import com.XD.fitgain.databinding.FragmentActivityBinding
 import kotlinx.android.synthetic.main.fragment_activity.*
+import kotlin.math.pow
 
 
 class Activity : Fragment() {
+    private lateinit var binding: FragmentActivityBinding
+
     private var currentSteps = 0
     private var previousSteps = 0
-    private var totalSteps = 0;
+    private var totalSteps = 0
+    private var totalDistance = 0.0
 
     private val stepsReceiver = object : BroadcastReceiver() {
         override fun onReceive(context: Context?, intent: Intent?) {
             if (intent != null) {
                 currentSteps = intent.extras?.get("steps").toString().toInt()
                 loadData()
-
             }
         }
     }
@@ -31,19 +35,25 @@ class Activity : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        binding = FragmentActivityBinding.inflate(inflater, container, false)
 
-        return inflater.inflate(R.layout.fragment_activity, container, false)
+        binding.btnInfo.setOnClickListener {
+            val builder = AlertDialog.Builder(activity)
+            with(builder)
+            {
+                setTitle(getString(com.XD.fitgain.R.string.title_oms_info))
+                setMessage(getString(com.XD.fitgain.R.string.oms_info))
+                show()
+            }
+        }
+
+        return binding.root
     }
 
     override fun onStart() {
         super.onStart()
         requireActivity().registerReceiver(stepsReceiver, getIntentFilter())
         loadData()
-
-        //Log.d("STEPS_DEBUG", sharedPreferences.getInt("stepCount", 0).toString())
-        //Log.d("STEPS_DEBUG", "Pasos anteriores $previousSteps")
-        //Log.d("STEPS_DEBUG", "Pasos actuales $currentSteps")
-
     }
 
     override fun onResume() {
@@ -63,7 +73,7 @@ class Activity : Fragment() {
 
     private fun saveData() {
         var sharedPreferences: SharedPreferences =
-            requireActivity().getSharedPreferences("myPref", MODE_PRIVATE)
+            requireActivity().getSharedPreferences("FitnessData", MODE_PRIVATE)
         val editor = sharedPreferences.edit()
         editor.clear()
         editor.putInt("stepCount", totalSteps)
@@ -72,17 +82,66 @@ class Activity : Fragment() {
 
     private fun loadData() {
         val sharedPreferences: SharedPreferences =
-            requireActivity().getSharedPreferences("myPref", MODE_PRIVATE)
+            requireActivity().getSharedPreferences("FitnessData", MODE_PRIVATE)
         previousSteps = sharedPreferences.getInt("stepCount", 0)
         totalSteps = previousSteps + currentSteps
 
-        var distance = (totalSteps * 0.71777203560149) / 1000
-        tv_disValue.text = String.format("%.2f", distance) + " Km"
+        //Distance setting values
+        totalDistance = (totalSteps * 0.71777203560149) / 1000
+        tv_disValue.text = String.format("%.2f", totalDistance) + " Km"
+        pgb_distance.apply {
+            setProgressWithAnimation(totalDistance.toFloat())
+        }
 
+        //Steps setting values
         tv_steps.text = totalSteps.toString()
         progress_circular.apply {
             setProgressWithAnimation(totalSteps.toFloat())
         }
+
+        //Goal setting value
+        var goal = 2500
+        tv_percentage_steps.text = String.format(
+            "%.2f",
+            (totalSteps.toFloat() / goal.toFloat()) * 100
+        ) + " % de tu meta diaria"
+
+        //Calories Burned
+        var weight = 90.0 //kg
+        var height = 1.82.pow(2) //m al cuadrado
+        var bmi = weight / height
+
+        var age = 16
+        var speedFactor = 3.0 //average for walk
+
+        var caloriesBurned = (totalSteps * 0.04 * bmi * age * speedFactor) / 1000
+
+        tv_calValue.text = String.format(
+            "%.2f",
+            caloriesBurned
+        ) + " Cal"
+
+        pgb_calories.apply {
+            setProgressWithAnimation(caloriesBurned.toFloat())
+        }
+
+        //Points conversion
+        val goalPoints = 10
+        var points = 0.001 * totalSteps
+        tv_poinstValue.text = String.format(
+            "%.2f",
+            points
+        ) + " PS"
+
+        tv_pointsInfo.text = String.format(
+            "%.0f",
+            points
+        )
+
+        pgb_limitedPoints.apply {
+            setProgressWithAnimation(points.toFloat())
+        }
+
     }
 
     private fun getIntentFilter(): IntentFilter {

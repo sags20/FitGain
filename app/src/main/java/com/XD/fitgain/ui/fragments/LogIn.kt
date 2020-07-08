616bcac5f5218352d8e6b4ca3e9bae0819c822df
sags20 <44414301+sags20@users.noreply.github.com>
2020-07-07 22:06:14 -0600

    Auth email/password
    Auth email/password

--- app/src/main/java/com/XD/fitgain/ui/fragments/LogIn.kt
+++ app/src/main/java/com/XD/fitgain/ui/fragments/LogIn.kt
@@ -1,7 +1,9 @@
 package com.XD.fitgain.ui.fragments
 
+import android.content.ContentValues.TAG
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -10,14 +12,21 @@ import androidx.navigation.findNavController
 import com.XD.fitgain.R
 import com.XD.fitgain.databinding.FragmentLogInBinding
 import com.XD.fitgain.ui.NavigationContainerHome
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.fragment_log_in.*
 
 class LogIn : Fragment() {
     private lateinit var binding: FragmentLogInBinding
+    private var mAuth: FirebaseAuth? = null
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentLogInBinding.inflate(inflater, container, false)
+        mAuth = FirebaseAuth.getInstance();
 
         binding.btnBackScreen.setOnClickListener {
             it.findNavController().navigate(R.id.action_logIn_to_logOut)
@@ -29,10 +38,27 @@ class LogIn : Fragment() {
 
         binding.btnLogin.setOnClickListener {
             startActivity(Intent(activity, NavigationContainerHome::class.java))
+            mAuth!!.signInWithEmailAndPassword(et_correo.text.toString(), et_password.text.toString())
+                .addOnCompleteListener(this,
+                    OnCompleteListener<AuthResult?> { task ->
+                        if (task.isSuccessful) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithEmail:success")
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithEmail:failure", task.exception)
+                        }
+
+                    })
+
         }
 
         return binding.root
     }
 
+
+}
+
+private fun Any.addOnCompleteListener(logIn: LogIn, onCompleteListener: OnCompleteListener<AuthResult?>) {
 
 }
--- app/src/main/java/com/XD/fitgain/ui/fragments/Register1.kt
+++ app/src/main/java/com/XD/fitgain/ui/fragments/Register1.kt
@@ -1,6 +1,8 @@
 package com.XD.fitgain.ui.fragments
 
+import android.content.ContentValues.TAG
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -8,15 +10,22 @@ import android.view.ViewGroup
 import androidx.navigation.findNavController
 import com.XD.fitgain.R
 import com.XD.fitgain.databinding.FragmentRegister1Binding
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.fragment_register1.*
 
 class Register1 : Fragment() {
 
     private lateinit var binding: FragmentRegister1Binding
+    private var mAuth: FirebaseAuth? = null
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentRegister1Binding.inflate(inflater, container, false)
+        mAuth = FirebaseAuth.getInstance();
 
         binding.btnBackScreen.setOnClickListener {
             it.findNavController().navigate(R.id.action_register1_to_logOut)
@@ -24,9 +33,25 @@ class Register1 : Fragment() {
 
         binding.btnContinuar.setOnClickListener {
             it.findNavController().navigate(R.id.action_register1_to_register2)
+            mAuth!!.createUserWithEmailAndPassword(et_correo.text.toString(), et_password.text.toString())
+                .addOnCompleteListener(this,
+                    OnCompleteListener<AuthResult?> { task ->
+                        if (task.isSuccessful) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "createUserWithEmail:success")
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.exception)
+                        }
+
+                    })
         }
 
         return binding.root
     }
+
+}
+
+private fun Any.addOnCompleteListener(register1: Register1, onCompleteListener: OnCompleteListener<AuthResult?>) {
 
 }
